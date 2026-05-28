cb34e5b339572863b0d2e7950d994f0f46a0b4dc
rskaindoy <rskaindoy@up.edu.ph>
2026-05-29 00:54:25 +0800

    feat: added instruc in  app.py

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-# connects backend to frontend
+'''
+- connects backend to frontend
+- reads and handles the data and logic 
+- sends data to HTML via render_template
+'''
+
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# input dictionary data here
+# accesing data from json files
+with open("data/pets.json") as f:
+    pets = json.load(f)
 
+with open("data/location.json") as f:
+    locations = json.load(f)
+
+with open("data/time.json") as f:
+    times = json.load(f)
+
+# input dictionary data here
+reports = {
+  "Cassie": {
+    "7AM-9:59AM": {
+      "Library": 3,
+      "Canteen": 1
+    },
+    "10AM-12:59PM": {
+      "Library": 2
+    }
+  }
+}
 
 # insert logic here
 
@@ -16,7 +42,7 @@ def home():
 
 @app.route("/report")
 def report():
-    return render_template("report.html")
+    return render_template("report.html", pets=pets, locations=locations, times=times)
 
 @app.route("/search")
 def search():
@@ -26,18 +52,33 @@ def search():
 # submit new report
 @app.route("/submit-report", methods=["POST"])
 def submit_report():
-    pass
-    # new data
-    # create pet/loc/time if not existing in dictionary
+    data = request.json
 
+    pet = data["pet"]
+    time = data["time"]
+    location = data["location"]
+
+    reports.setdefault(pet, {})
+    reports[pet].setdefault(time, {})
+    reports[pet][time].setdefault(location, 0)
+    reports[pet][time][location] += 1
+
+    return {"message": "Report saved"}
 
 # search pet
 @app.route("/search-pet", methods=["POST"])
 def search_pet():
     data = request.json
-    pet = data['pet']
-    results = compute(pet)
-    return jsonify(results)     
+    pet = data["pet"]
+    time = data["time"]
+
+    if pet not in reports or time not in reports[pet]:
+        return []
+    
+    locations = reports[pet][time]      # returns { key-value pairs }
+
+    # call helper func for logic
+    # return json format of logic results (ranked list of tuples)
 
 # run app
 if __name__ == "__main__":
